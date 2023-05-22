509755a47af0aecb113a0a0c05d4bb350fa63869
diendasls <101309423+diendasls@users.noreply.github.com>
2023-05-22 21:53:02 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+// import screen
+import 'package:frontend_tubes/view/onboard_screen.dart';
+import 'package:frontend_tubes/view/login_screen.dart';
+import 'package:frontend_tubes/view/signup_screen.dart';
+import 'package:frontend_tubes/view/beranda_investor_screen.dart';
+import 'package:frontend_tubes/view/beranda_member_screen.dart';
+import 'package:frontend_tubes/view/beranda_visitor_screen.dart';
+
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  runApp(MyApp());
+}
+
+// pindah file route.dart (REMINDER)
+class AppRoutes {
+  static const String onboardScreen = '/onboard_screen';
+  static const String loginScreen = '/login_screen';
+  static const String signupScreen = '/signup_screen';
+  static const String berandaInvestorScreen = '/beranda_investor_screen';
+  static const String berandaMemberScreen = '/beranda_member_screen';
+  static const String berandaVisitorScreen = '/beranda_visitor_screen';
+
+
+  static Map<String, WidgetBuilder> routes = {
+    onboardScreen: (context) => OnboardScreen(),
+    loginScreen: (context) => LoginScreen(),
+    signupScreen: (context) => SignupScreen(),
+    berandaInvestorScreen: (context) => BerandaInvestorScreen(),
+    berandaMemberScreen:(context) => BerandaMemberScreen(),
+    berandaVisitorScreen:(context) => BerandaVisitorScreen(),
+  };
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        visualDensity: VisualDensity.standard,
+        // fontFamily: 'Poppins',
+      ),
+      title: 'Tubes',
+      debugShowCheckedModeBanner: false,
+      initialRoute: AppRoutes.onboardScreen,
+      routes: AppRoutes.routes,
+    );
+  }
+}
