98ee5ea535ca64fc2cd7c14121b1351645286981
Salsabila Kanaya <salsabilak28@upi.edu>
2023-06-15 15:40:25 +0700

    commit

--- database.py
+++ database.py
@@ -3,57 +3,53 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 c = conn.cursor()
 
-create_table = '''
-    CREATE TABLE users (
+create_table_users = '''
+    CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         nama_lengkap TEXT NOT NULL,
         email TEXT NOT NULL,
         password TEXT NOT NULL,
         no_hp TEXT NOT NULL,
+        username TEXT NOT NULL,
         alamat TEXT NOT NULL,
         pekerjaan TEXT NOT NULL,
         no_ktp TEXT NOT NULL,
-        npwp TEXT NOT NULL,
+        npwp INTEGER NOT NULL,
         pendapatan INTEGER NOT NULL,
         sumber_pendapatan TEXT NOT NULL,
-        rekening_bank TEXT NOT NULL
+        rekening_bank TEXT NOT NULL,
+        jenis_umkm TEXT NOT NULL
     )
 '''
 
-c.execute(create_table)
+c.execute(create_table_users)
 conn.commit()
-conn.close()
-
-# Fungsi untuk memeriksa apakah pengguna dengan email tertentu sudah terdaftar
-def is_user_registered(email):
-    conn = sqlite3.connect('database.db')
-    c = conn.cursor()
-    c.execute("SELECT COUNT(*) FROM users WHERE email=?", (email,))
-    result = c.fetchone()
-    conn.close()
-    return result[0] > 0
-
-# Fungsi untuk menyimpan data pengguna yang baru terdaftar
-def save_user(full_name, email, password, phone_number, address, occupation, id_card_number, npwp, income, income_source, bank_account):
-    conn = sqlite3.connect('database.db')
-    c = conn.cursor()
-    c.execute("INSERT INTO users (full_name, email, password, phone_number, address, occupation, id_card_number, npwp, income, income_source, bank_account) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-              (full_name, email, password, phone_number, address, occupation, id_card_number, npwp, income, income_source, bank_account))
-    conn.commit()
-    conn.close()
-
-
-conn = sqlite3.connect('database.db')
-c = conn.cursor()
 
 create_table_chat= '''
-    CREATE TABLE messages (
+    CREATE TABLE IF NOT EXISTS messages (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         sender TEXT NOT NULL,
         content TEXT NOT NULL
     )
 '''
 
-c.execute(create_table)
+c.execute(create_table_chat)
+conn.commit()
+
+    # membuat tabel postingan
+create_table_postingan='''
+    CREATE TABLE IF NOT EXISTS postingan(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            maksimum_pendanaan STRING,
+            bagi_hasil STRING,
+            tenor STRING,
+            pendanaan_ke INTEGER,
+            jenis_angsuran STRING,
+            jumlah_angsuran STRING,
+            penghasilan_perbulan STRING
+    )
+'''
+    
+c.execute(create_table_postingan)
 conn.commit()
 conn.close()
